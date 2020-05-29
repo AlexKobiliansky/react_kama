aee9f0e2688453da8373b33a540fa8daeb4341e7
alex <alex_kobilianskiy@bigmir.net>
2020-05-29 10:32:04 +0300

    lesson 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <nav className='nav'>
+            <div><a href="#0">Profile</a></div>
+            <div><a href="#0">Messages</a></div>
+            <div><a href="#0">News</a></div>
+            <div><a href="#0">Music</a></div>
+            <div><a href="#0">Settings</a></div>
+        </nav>
+        )
+
+}
+
+export default Header;
