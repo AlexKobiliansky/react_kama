87d844f49fc8c9328bcbdac45c23b508462cdb25
alex <alex_kobilianskiy@bigmir.net>
2020-10-30 20:07:41 +0200

    after lesson 58

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,44 +1,29 @@
 import React from "react";
-import styles from './Users.module.css'
+import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/no-avatar.png";
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://s3.cdn.teleprogramma.pro/wp-content/uploads/2020/01/f9cd095e4b8a640631410b9dbddf5caf.jpg',
-                followed: true,
-                fullName: 'Alex',
-                status: 'I am a boss',
-                location: {city: 'Cherkassy', country: 'Ukraine'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://s3.cdn.teleprogramma.pro/wp-content/uploads/2020/01/f9cd095e4b8a640631410b9dbddf5caf.jpg',
-                followed: false,
-                fullName: 'Anna',
-                status: 'I am not a boss',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://s3.cdn.teleprogramma.pro/wp-content/uploads/2020/01/f9cd095e4b8a640631410b9dbddf5caf.jpg',
-                followed: false,
-                fullName: 'Sasha',
-                status: 'bla bla bla',
-                location: {city: 'Kyiv', country: 'Ukraine'}
-            },
-        ])
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i=1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-
-    return (
+    return  (
         <div>
+            <div>
+                {pages.map( p => {
+                    return <span className={props.currentPage === p && styles.selectedPage}
+                                 onClick={(e) => {props.onPageChanged(p)}}>{p}</span>
+                })}
+            </div>
+
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} alt="alt" className={styles.userPhoto}/>
+                            <img src={ u.photos.small != null ? u.photos.small : userPhoto} alt="alt" className={styles.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
@@ -48,12 +33,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
                 </div>)
--- src/redux/users-reducer.jsx
+++ src/redux/users-reducer.jsx
@@ -1,13 +1,16 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialState = {
-    users: [
-        // {id: 1, photoUrl: 'https://s3.cdn.teleprogramma.pro/wp-content/uploads/2020/01/f9cd095e4b8a640631410b9dbddf5caf.jpg' ,followed: true, fullName: 'Alex', status: 'I am a boss', location: {city: 'Cherkassy', country: 'Ukraine'}},
-        // {id: 2, photoUrl: 'https://s3.cdn.teleprogramma.pro/wp-content/uploads/2020/01/f9cd095e4b8a640631410b9dbddf5caf.jpg' ,followed: false, fullName: 'Anna', status: 'I am not a boss', location: {city: 'Moscow', country: 'Russia'}},
-        // {id: 3, photoUrl: 'https://s3.cdn.teleprogramma.pro/wp-content/uploads/2020/01/f9cd095e4b8a640631410b9dbddf5caf.jpg' ,followed: false, fullName: 'Sasha', status: 'bla bla bla', location: {city: 'Kyiv', country: 'Ukraine'}},
-    ]
+    users: [],
+    pageSize: 100,
+    totalUsersCount: 0,
+    currentPage: 1,
+    isFetching: false
 }
 
 const usersReducer = (state=initialState, action) => {
@@ -34,14 +37,23 @@ const usersReducer = (state=initialState, action) => {
                 })
             }
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users: action.users}
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage}
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalUsersCount: action.count}
+        case TOGGLE_IS_FETCHING:
+            return {...state, isFetching: action.isFetching}
         default:
             return state;
     }
 }
 
-export const followAC = (userId) => ({type: FOLLOW, userId})
-export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
-export const setUsersAC = (users) => ({type: SET_USERS, users})
+export const follow = (userId) => ({type: FOLLOW, userId})
+export const unfollow = (userId) => ({type: UNFOLLOW, userId})
+export const setUsers = (users) => ({type: SET_USERS, users})
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCount = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
+export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching})
 
 export default usersReducer;
