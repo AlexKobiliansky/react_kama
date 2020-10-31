88bc7979d08d739dfd315edd2cfcb81723afefe3
alex <alex_kobilianskiy@bigmir.net>
2020-10-31 16:31:11 +0200

    after lesson 61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-function Header(){
+function Header(props){
     return (
         <header className={s.header}>
             <img src="https://dcassetcdn.com/design_img/1559024/551167/551167_7840631_1559024_911ff84c_image.png" alt="alt"/>
+
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+
+            </div>
         </header>
         )
 
