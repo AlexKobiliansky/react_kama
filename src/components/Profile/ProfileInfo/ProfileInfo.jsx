f6470cced71e2498d4f4d5291b85171d88136bae
alex <alex_kobilianskiy@bigmir.net>
2020-11-07 22:57:06 +0200

    after lesson 71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloader";
+import ProfileStatus from './ProfileStatus';
 
 function ProfileInfo(props){
     if(!props.profile) {
@@ -8,12 +9,12 @@ function ProfileInfo(props){
     }
     return (
         <div>
-            <div>
-                <img src="https://static01.nyt.com/images/2019/11/05/science/28TB-SUNSET1/merlin_163473282_fe17fc6b-78b6-4cdd-b301-6f63e6ebdd7a-superJumbo.jpg" alt="alt" className={s.wrap__img}/>
-            </div>
+            {/*<div>*/}
+            {/*    <img src="https://static01.nyt.com/images/2019/11/05/science/28TB-SUNSET1/merlin_163473282_fe17fc6b-78b6-4cdd-b301-6f63e6ebdd7a-superJumbo.jpg" alt="alt" className={s.wrap__img}/>*/}
+            {/*</div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} alt="alt"/>
-                ava + description
+                <ProfileStatus status="Hello, my dear friend!!!" />
             </div>
         </div>
         )
