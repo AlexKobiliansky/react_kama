a8a3c2bfbe30373940469014808963a59f79db44
alex <alex_kobilianskiy@bigmir.net>
2020-11-27 13:35:36 +0200

    after lesson 78

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import {Field, reduxForm} from "redux-form";
 import {Input} from "../Common/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router-dom";
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
                 <Field
-                    name={"login"}
-                    placeholder="login"
+                    name={"email"}
+                    placeholder="Email"
                     component={Input}
                     validate={[required]}/>
             </div>
             <div>
                 <Field
+                    type={"password"}
                     name={"password"}
-                    placeholder="password"
+                    placeholder="Password"
                     component={Input}
                     validate={[required]}/>
             </div>
@@ -38,7 +42,11 @@ const LoginReduxForm = reduxForm({
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        console.log(formData)
+        props.login(formData.email, formData.password, formData.rememberMe)
+    }
+
+    if (props.isAuth) {
+        return <Redirect to={"/profile"} />
     }
 
     return (
@@ -49,4 +57,8 @@ const Login = (props) => {
     )
 }
 
-export default Login
+
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+export default connect(mapStateToProps, {login})(Login)
